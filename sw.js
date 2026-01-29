aa8c0be1b7b7fdfcb5b72458515048e8a096aa59
dasilvalimaluizfilipe-lab <dasilvalimaluizfilipe@gmail.com>
2026-01-29 15:57:16 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("goodsounds-v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/home.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
